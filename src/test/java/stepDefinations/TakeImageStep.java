94e890d1a277cf7e061cb9bbea1269874485020b
nishaslidetech <nisha.dhiman@slidetech.in>
2022-09-13 17:24:24 +0530

    first commit

---
+++ src/test/java/stepDefinations/TakeImageStep.java
@@ -0,0 +1,27 @@
+package stepDefinations;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import SetUpClass.BaseClass;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+
+public class TakeImageStep extends BaseClass {
+	@After
+	public void tearDown(Scenario scenario) {
+		if (scenario.isFailed()) {
+			// String screenshotName = scenario.getName().replaceAll(" ", "_");
+			try {
+				File srcPath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+				byte[] newFile = FileUtils.readFileToByteArray(srcPath);
+				scenario.attach(newFile, "image/png", "screenshotName");
+			} catch (IOException e) {
+			}
+		}
+	}
+}
